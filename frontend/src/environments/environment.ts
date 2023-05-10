40ea88ad569bd894ed397a7cd22985eb13db00e1
sachinka2109 <101002628+sachinka2109@users.noreply.github.com>
2023-05-11 00:39:02 +0530

    Update environment.ts
    Removed env variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  // REST_API : 'https://mean-ecom.onrender.com/api/',
-  // SOCKET_URL : 'https://mean-ecom.onrender.com',
   REST_API : 'http://localhost:3000/api/',
   SOCKET_URL : 'http://localhost:3000/',
   STRIPE_KEY:  'Your Stripe-Primary-Key',
